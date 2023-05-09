ca9c41a692d9502ff96e1a85648d115e693168a8
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-05-09 08:55:12 +0800

    [i] Install Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//Components
+import Layout from "./pages/Layout";
+import Patient from "./pages/Patient";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Patient />}/>
+          <Route path="patient" element={<Patient />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
