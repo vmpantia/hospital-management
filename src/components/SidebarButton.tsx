8fa219e5edf8a82887f219c9f812fd874bc5b7f7
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-05-09 09:53:50 +0800

    [u] Changes

--- src/components/SidebarButton.tsx
+++ src/components/SidebarButton.tsx
@@ -8,8 +8,8 @@ type Props = {
 const SidebarButton = (props:Props) => {
     const {text} = props;
   return (
-    <a className='flex p-1 pl-3 mb-1 rounded hover:bg-slate-600'>
-        <UsersIcon className='h-4 w-4 mr-4 mt-1'/>
+    <a className='flex p-2 pl-3 mb-1 text-sm rounded hover:bg-slate-600'>
+        <UsersIcon className='h-4 w-4 mr-4 mt-0.5'/>
         {text}
     </a>
   )
